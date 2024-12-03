b603ee756bab6c143e58a0c17aa2becdcda61c25
rmonishbalu <rmonishmoni@gmail.com>
2024-12-03 11:59:37 +0530

    initial file added

---
+++ src/common/sidebar.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+function DashboardSidebar() {
+    return (
+        <>
+            <aside className="sidebar">
+                {/* sidebar close btn */}
+                <button type="button" className="sidebar-close-btn text-gray-500 hover-text-white hover-bg-main-600 text-md w-24 h-24 border border-gray-100 hover-border-main-600 d-xl-none d-flex flex-center rounded-circle position-absolute"><i className="ph ph-x" /></button>
+                {/* sidebar close btn */}
+                <a href="index.html" className="sidebar__logo text-center p-20 position-sticky inset-block-start-0 bg-white w-100 z-1 pb-10">
+                    <img src="assets/images/logo/logo.png" alt="Logo" />
+                </a>
+                <div className="sidebar-menu-wrapper overflow-y-auto scroll-sm">
+                    <div className="p-20 pt-10">
+                        <ul className="sidebar-menu">
+                            <li className="sidebar-menu__item ">
+                                <a href="javascript:void(0)" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-squares-four" /></span>
+                                    <span className="text">Dashboard</span>
+                                </a>
+                               
+                            </li>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <a href="javascript:void(0)" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-graduation-cap" /></span>
+                                    <span className="text">Courses</span>
+                                </a>
+                                {/* Submenu start */}
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Create Courses </a>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Edit Courses </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <a href="javascript:void(0)" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-graduation-cap" /></span>
+                                    <span className="text">People</span>
+                                </a>
+                                {/* Submenu start */}
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Trainer </a>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Student </a>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Admin </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <a href="javascript:void(0)" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-graduation-cap" /></span>
+                                    <span className="text">Meeting</span>
+                                </a>
+                                {/* Submenu start */}
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> My meetings </a>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Shedule New </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <a href="javascript:void(0)" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-graduation-cap" /></span>
+                                    <span className="text">Payments</span>
+                                </a>
+                                {/* Submenu start */}
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Payment Keys </a>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <a href="" className="sidebar-submenu__link"> Transactions </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="p-20 pt-80">
+                        <div className="bg-main-50 p-20 pt-0 rounded-16 text-center mt-74">
+                            <span className="border border-5 bg-white mx-auto border-primary-50 w-114 h-114 rounded-circle flex-center text-success-600 text-2xl translate-n74">
+                                <img src="assets/images/icons/certificate.png" alt className="centerised-img" />
+                            </span>
+                            <div className="mt-n74">
+                                <h5 className="mb-4 mt-22">Get Pro Certificate</h5>
+                                <p className>Explore 400+ courses with lifetime members</p>
+                                <a href="pricing-plan.html" className="btn btn-main mt-16 rounded-pill">Get Access</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </>
+    );
+}
+
+export default DashboardSidebar;
---
+++ src/componets/home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import DashboardSidebar from "../common/sidebar";
+import Header from "../common/header";
+import Homebody1 from "../layout/home/homebody1";
+import Homebody2 from "../layout/home/homebody2";
+
+function Home() {
+    return (
+        <>
+            <div>
+                <DashboardSidebar />
+
+                <div className="dashboard-main-wrapper">
+                    <Header />
+                    <div className="dashboard-body">
+                        <div className="row gy-4">
+                            <div className="col-lg-9">
+                                {/* Widgets Start */}
+                                <Homebody1 />
+                                {/* Top Course Start */}
+                                <Homebody2 />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dashboard-footer">
+                        <div className="flex-between flex-wrap gap-16">
+                            <p className="text-gray-300 text-13 fw-normal"> © Copyright Edmate 2024, All Right Reserverd</p>
+                            <div className="flex-align flex-wrap gap-16">
+                                <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">License</a>
+                                <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">More Themes</a>
+                                <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">Documentation</a>
+                                <a href="#" className="text-gray-300 text-13 fw-normal hover-text-main-600 hover-text-decoration-underline">Support</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
