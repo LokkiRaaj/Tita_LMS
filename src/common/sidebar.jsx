bd64c70eecebb721be306d45ef74e3addf38794b
rmonishbalu <rmonishmoni@gmail.com>
2024-12-13 10:45:18 +0530

    aa

--- src/common/sidebar.jsx
+++ src/common/sidebar.jsx
@@ -1,104 +1,105 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LuFileBadge2 } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
 
 function DashboardSidebar() {
     return (
         <>
-
-
-            <aside class="sidebar">
-
-                <button type="button"
-                    class="sidebar-close-btn text-gray-500 hover-text-white hover-bg-main-600 text-md w-24 h-24 border border-gray-100 hover-border-main-600 d-xl-none d-flex flex-center rounded-circle position-absolute"><i
-                        class="ph ph-x"></i></button>
-
-
-                <a href="index.html"
-                    class="sidebar__logo text-center p-20 position-sticky inset-block-start-0 bg-white w-100 z-1 pb-10">
+            <aside className="sidebar">
+                <button type="button" className="sidebar-close-btn" style={{ float: 'right' }}>
+                    <span className="font-bold"><IoMdClose /></span>
+                </button>
+                <a href="" className="sidebar__logo text-center p-20 position-sticky inset-block-start-0 bg-white w-100 z-1 pb-10">
                     <img src="assets/images/logo/logo.png" alt="Logo" />
                 </a>
 
-                <div class="sidebar-menu-wrapper overflow-y-auto scroll-sm">
-                    <div class="p-20 pt-10">
-                        <ul class="sidebar-menu">
-
-                            <a href="javascript:void(0)" class="sidebar-menu__link">
-                                <span class="icon"><i class="ph ph-squares-four"></i></span>
-                                <a href="/" ><span class="text">Dashboard</span></a>
-                            </a>
-                            <li class="sidebar-menu__item has-dropdown">
-                                <a href="javascript:void(0)" class="sidebar-menu__link">
-                                    <span class="icon"><i class="ph ph-graduation-cap"></i></span>
-                                    <span class="text">Courses</span>
-                                </a>
-                                <ul class="sidebar-submenu">
-                                    <li class="sidebar-submenu__item">
-                                        <a href="/create-courses" class="sidebar-submenu__link"> Create Courses </a>
+                <div className="sidebar-menu-wrapper overflow-y-auto scroll-sm">
+                    <div className="p-20 pt-10">
+                        <ul className="sidebar-menu">
+                            <li className="sidebar-menu__item">
+                                <Link to="/" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-squares-four"></i></span>
+                                    <span className="text">Dashboard</span>
+                                </Link>
+                            </li>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <Link to="" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-graduation-cap"></i></span>
+                                    <span className="text">Courses</span>
+                                </Link>
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="/create-courses" className="sidebar-submenu__link">Create Courses</Link>
                                     </li>
-                                    <li class="sidebar-submenu__item">
-                                        <a href="#" class="sidebar-submenu__link"> Edit Courses </a>
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="/edit-courses" className="sidebar-submenu__link">Edit Courses</Link>
                                     </li>
                                 </ul>
                             </li>
 
-                            <li class="sidebar-menu__item has-dropdown">
-                                <a href="javascript:void(0)" class="sidebar-menu__link">
-                                    <span class="icon"><i class="ph ph-users-three"></i></span>
-                                    <span class="text">People</span>
-                                </a>
-                                <ul class="sidebar-submenu">
-                                    <li class="sidebar-submenu__item">
-                                        <a href="/trainer" class="sidebar-submenu__link"> Trainer </a>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <Link to="" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-users-three"></i></span>
+                                    <span className="text">People</span>
+                                </Link>
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="/trainer" className="sidebar-submenu__link">Trainer</Link>
                                     </li>
-                                    <li class="sidebar-submenu__item">
-                                        <a href="#" class="sidebar-submenu__link"> Student </a>
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="#" className="sidebar-submenu__link">Student</Link>
                                     </li>
-                                    <li class="sidebar-submenu__item">
-                                        <a href="#" class="sidebar-submenu__link"> Admin </a>
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="#" className="sidebar-submenu__link">Admin</Link>
                                     </li>
                                 </ul>
-                                </li>
+                            </li>
 
-                            <li class="sidebar-menu__item has-dropdown">
-                                <a href="javascript:void(0)" class="sidebar-menu__link">
-                                    <span class="icon"><i class="ph ph-calendar-blank"></i></span>
-                                    <span class="text">Meeting</span>
-                                </a>
-                                <ul class="sidebar-submenu">
-                                    <li class="sidebar-submenu__item">
-                                        <a href="/my-meeting" class="sidebar-submenu__link"> My Meeting </a>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <Link to="#" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-calendar-blank"></i></span>
+                                    <span className="text">Meeting</span>
+                                </Link>
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="/my-meeting" className="sidebar-submenu__link">My Meeting</Link>
                                     </li>
-                                  <li class="sidebar-submenu__item">
-                                <a href="/new-schedule" class="sidebar-submenu__link"> New Schedule </a>
-                            </li>
-                                </ul>
-                            </li>
-                            <li class="sidebar-menu__item has-dropdown">
-                                <a href="javascript:void(0)" class="sidebar-menu__link">
-                                    <span class="icon"><i class="ph ph-credit-card"></i></span>
-                                    <span class="text">Payments</span>
-                                </a>
-                                <ul class="sidebar-submenu">
-                                    <li class="sidebar-submenu__item">
-                                        <a href="#" class="sidebar-submenu__link"> Payment Keys </a>
-                                    </li>
-                                    <li class="sidebar-submenu__item">
-                                        <a href="#" class="sidebar-submenu__link"> Transactions </a>
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="/new-schedule" className="sidebar-submenu__link">New Schedule</Link>
                                     </li>
                                 </ul>
                             </li>
-                            <a href="javascript:void(0)" class="sidebar-menu__link">
-                                <span class="icon"><i class="ph ph-certificate"></i></span>
-                                <span class="text">Batch and Certification</span>
-                            </a>
+                            <li className="sidebar-menu__item has-dropdown">
+                                <Link to="#" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-credit-card"></i></span>
+                                    <span className="text">Payments</span>
+                                </Link>
+                                <ul className="sidebar-submenu">
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="#" className="sidebar-submenu__link">Payment Keys</Link>
+                                    </li>
+                                    <li className="sidebar-submenu__item">
+                                        <Link to="#" className="sidebar-submenu__link">Transactions</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="sidebar-menu__item">
+                                <Link to="/badge" className="sidebar-menu__link">
+                                    <span className="icon"><LuFileBadge2 /></span>
+                                    <span className="text">Badge</span>
+                                </Link>
+                            </li>
+                            <li className="sidebar-menu__item">
+                                <Link to="/certification" className="sidebar-menu__link">
+                                    <span className="icon"><i className="ph ph-certificate"></i></span>
+                                    <span className="text">Certification</span>
+                                </Link>
+                            </li>
                         </ul>
                     </div>
-
                 </div>
-
             </aside>
-
-
         </>
     );
 }
